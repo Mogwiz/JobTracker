33d6847a3bc902431aedae45cbb5af1f11df6d1d
Mogwiz <166713004+Mogwiz@users.noreply.github.com>
2024-07-29 17:30:39 +0200

    JT - 3

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,26 @@
+const User = require('../model/user')
+
+// Handle errors
+const handleErrors = (err) =>{
+    console.log(err.message, err.code)
+    let errors = {firstName: '', lastName: '', email: '', password: '', github: ''}
+
+    // Duplicate error code
+    if(err.code === 11000) {
+        errors.email = 'That email is already registered'
+        return errors
+    }
+
+    // Validation errors
+    if (err.message.includes('User validation failed')) {
+        Object.values(err.errors).forEach(({properties}) =>{
+            errors[properties.path] = properties.message
+        })
+    }
+
+    return errors
+}
+
 module.exports.signup_get = (req, res) =>{
     res.render('signup')
 }
@@ -6,14 +29,20 @@ module.exports.login_get = (req, res) =>{
     res.render('signup')
 }
 
-module.exports.signup_post = (req, res) =>{
-    const { email, password} = req.body
+module.exports.signup_post = async (req, res) =>{
+    const { firstName, lastName, email, password, github } = req.body
 
-    console.log(email, password)
-    res.send('User logged in !')
+    try {
+        const user = await User.create({ firstName, lastName, email, password, github })
+        res.status(201).json(user)
+    } 
+    catch(err) {
+        const errors = handleErrors(err)
+        res.status(400).json({ errors })
+    }
 }
 
-module.exports.login_post = (req, res) =>{
+module.exports.login_post = async (req, res) =>{
     const { email, password} = req.body
 
     console.log(email, password)
--- model/user.js
+++ model/user.js
@@ -1,26 +1,31 @@
 const mongoose = require('mongoose')
+const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
+        required: [true, 'Please enter your first name'],
     },
     lastName: {
         type: String,
-        required: true,
+        required: [true, 'Please enter your last name'],
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter your email'],
         lowercase: true,
+        unique: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minlength: [6, 'Minimum password length is 6 characters'],
     },
     github: {
         type: String,
         lowercase: true,
-    },
-    password: {
-        type: String,
-        required: true,
     },
     createdAt: {
         type: Date,
@@ -31,6 +36,19 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: () => Date.now(),
     },
+})
+
+// Function after user saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('New user was created and saved', doc)
+    next()
+})
+
+//Function before user saved to db
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model("User", userSchema)
