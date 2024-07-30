8c559d55b96c3460d24c048e89a3aa2507ca47d3
mog <lbentbagdad@gmail.com>
2024-07-30 11:31:06 +0200

    JT - 6

--- main.js
+++ main.js
@@ -51,23 +51,3 @@ app.get('/job', (req, res) =>{
 })
 
 app.use(authRoutes)
-
-// Cookies
-
-app.get('/set-cookies', (req, res) =>{
-
-    res.cookie('newUser', false)
-    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
-
-    res.send('You got the cookies !')
-
-})
-
-app.get('/read-cookies', (req, res) =>{
-    
-    const cookies = req.cookies
-    console.log(cookies.newUser)
-
-    res.json(cookies)
-
-})
